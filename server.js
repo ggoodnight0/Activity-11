eb7c2a38c4b9f26ef40c902a202268f5b4bf3c29
ggoodnight0 <148833417+ggoodnight0@users.noreply.github.com>
2024-05-15 20:41:24 -0500

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
+const Note = require('./models/Note'); // Assuming you have a Note model defined
 const app = express();
 const PORT = process.env.PORT || 3000;
 
